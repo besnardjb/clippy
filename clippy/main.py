97bf2343e9da9e2e94df4c24bfa804eacf10eee4
BESNARD Jean-Baptiste <jbbesnard@paratools.com>
2023-08-11 22:52:14 +0200

    Commands

--- clippy/main.py
+++ clippy/main.py
@@ -6,11 +6,17 @@ import os
 from rich.console import Console
 from rich.markdown import Markdown
 import sys
+import subprocess
+import re
 
 console = Console()
 
 
 class Llama:
+
+
+   def _links2(self, url):
+      return subprocess.check_output(["links2", "-dump", url]).decode()
 
    def _check(self):
       try:
@@ -24,8 +30,14 @@ class Llama:
       if (user and not passwd) or (passwd and not user):
          raise Exception("Bot user and password must be set")
 
-      self.user = user
-      self.password = passwd
+      self.session = requests.Session()
+      if user:
+         self.session.auth = (user, passwd)
+
+      self.reman = r'\(MAN (\s+)\)'
+      self.reopen = r'\(OPEN\s+(https?://[^\s/$.?#].[^\s]*)\)'
+      self.reurl = r'\(GET\s+(https?://[^\s/$.?#].[^\s]*)\)'
+      self.line_count = 0
 
       self._check()
       if personality:
@@ -33,40 +45,54 @@ class Llama:
             self.personality = f.read()
       else:
          self.personality = """
-You are llama a coding assisant.
-You are here to make your best to answer questions from User.
-You make structured responses (possibly with subparts with markdown headings) and try to highlight all possibilities while remaining concise.
-You use as much markdown as possible in your answers as supported by commonmark.
-When outputing code do not forget to put the right language in the MD syntax, for example:
+# Identity
 
-```python
-print("hello assistant")
-```
+You are llama a resource aggregator and coding assistant.
+You help the user searching information on the Internet.
 
-Always put code in codeblocks.
-Never mention you use markdown just do it!
-Never say things such as please note that the above response is just an example and not a real answer.
-Do not infer what user would say.
+# Capabilities
 
+## The GET Command
 
-User: Hello!
-Llama: Hello dear user how can I help you ?
-User: Give me some examples of native and interpreted languages
-Llama:
-# Native
+To do so you can ask 'system' to issue HTTP requests for you
+by doing (GET URL). You always fetch information first using the command syntax below.
 
-- C : is a native language of reference
-- C++ : add object oriented notation to C
+System will respond with the content of the URL in HTML, you will only consider the content and not the code.
 
-## Interpreted
+As such feel free to send queries to online services and API such as Wikipedia, favor services with text content as you can only process text.
+No queries will be made unless you write the command.
 
-- Python: is a very popular language
-- Perl: uses the same VM as python
+You can then use keys from this JSON to search the actual content on wikipedia:
+(GET https://en.wikipedia.org/wiki/Python (programming language))
+System: content of the Python wikipedia page [...]
 
+Systematically do GET requests first to enrich information. Always put (GET URL) on a single line and wait for system's response.
+When parsing a system response do not react to the code but only to the content of the page.
 
+## The OPEN Command
 
+Another command is (OPEN URL) with this command you directly show the page to user:, avoiding to explain. You can use it for example to open google maps or other visual services.
 
+(OPEN http://google.com)
+
+will open google.com to the user, if it is the response he needed. It is a very good way to answer user's question to directly open a page if you cannot provide the answer directly.
+
+# General guidelines
+
+Always focus on the content and answering the user questions, not commenting the code.
+As google maps is visual always open is to the user with OPEN while pasing the correct parameters.
+When you don't know try a yahoo search and extract links.
+
+# Chat with the USER and System
+
+User: can you open google ?
+Llama: (OPEN http://google.com)
+User: Tell me more about Brest
+Llama: Let me look on Wikipedia to refresh my mind ... (GET http://en.wikipedia.com/wiki/Brest_France)
+System: [CONTENT of the BREST page]
+Lllama: Brest is a city in Brittany ...
       """
+
       self.ctx = [self.personality]
 
    def userprompt(self):
@@ -79,20 +105,74 @@ Llama:
       print("\033[{}A\033[J".format(lines_to_clear))
       console.print(md,end='',justify="left")
 
-   def query(self, query, single=False, md=True):
-      self.ctx.append("User: {}".format(query))
+   def parse_each_line(self, linebuff, md=False):
+      if "\n" in linebuff:
+         m = re.findall(self.reurl, linebuff)
+         if m:
+            for match in m:
+               print(f"Looking for {match} on the web ...")
+               self.line_count = self.line_count + 1
+               ret = self._links2(match)
+               ret_clean = self.blocking_query("""
+You are an assistant processing HTML pages for the user summarizing valuable informations from it.
+Your name is Llama.
+
+User: Hello how are you ?
+Llama: I'm fine what can I do for you ?
+User: Can you clean the content of this page to only return the content : \n\n""" + ret)
+               print(ret_clean)
+               self.query(ret_clean, single=True, md=md, issystem=True)
+         m = re.findall(self.reopen, linebuff)
+         if m:
+            for match in m:
+               print(f"Opening for {match} on the web ...")
+               self.line_count = self.line_count + 1
+               subprocess.call(["xdg-open", match])
+         return ""
+      return linebuff
+
+
+   def blocking_query(self, query):
+      resp = self.session.post("{}/completion".format(self.url), stream=True, json={
+                        "stream": False,
+                        "n_predict": 400,
+                        "temperature": 0.7,
+                        "stop" : [ "User:", "System"],
+                        # "repeat_last_n": 256,
+                        # "repeat_penalty": 1.18,
+                        # "top_k": 40,
+                        # "top_p": 0.5,
+                        # "tfs_z": 1,
+                        # "typical_p": 1,
+                        # "presence_penalty": 0,
+                        # "frequency_penalty": 0,
+                        "mirostat": 2,
+                        "mirostat_tau": 5,
+                        "mirostat_eta": 0.1,
+                        "prompt": query
+                        })
+
+      if resp.status_code != 200:
+         print(resp.text)
+         raise Exception("Had {} response: {}".format(resp.status_code, resp.reason))
+
+      data = resp.json()
+      return data["content"]
+
+
+   def query(self, query, single=False, md=True, issystem=False):
+      if issystem:
+         self.ctx.append("System: {}".format(query))
+      else:
+         self.ctx.append("User: {}".format(query))
+
       ctx = "\n".join(self.ctx)
 
-      session = requests.Session()
-      if self.user:
-         session.auth = (self.user, self.password)
-
-
-      resp = session.post("{}/completion".format(self.url), stream=True, json={
+      resp = self.session.post("{}/completion".format(self.url), stream=True, json={
                               "stream": True,
                               "n_predict": 400,
                               "temperature": 0.7,
-                              "stop" : [ "User:"],
+                              "stop" : [ "User:", "System"],
                               # "repeat_last_n": 256,
                               # "repeat_penalty": 1.18,
                               # "top_k": 40,
@@ -101,18 +181,21 @@ Llama:
                               # "typical_p": 1,
                               # "presence_penalty": 0,
                               # "frequency_penalty": 0,
-                              # "mirostat": 0,
-                              # "mirostat_tau": 5,
-                              # "mirostat_eta": 0.1,
+                              "mirostat": 2,
+                              "mirostat_tau": 5,
+                              "mirostat_eta": 0.1,
                               "prompt": ctx
                               })
 
       if resp.status_code != 200:
-         raise Exception("Had non 200 response {}".format(resp.status_code))
+         print(resp.text)
+         raise Exception("Had {} response: {}".format(resp.status_code, resp.reason))
 
       llamaresp = ""
 
-      line_count = 0
+
+      linebuff = ""
+
 
       for line in resp.iter_lines():
          if line:
@@ -121,16 +204,24 @@ Llama:
                   js = json.loads(rjs)
                   r = js["content"]
                   llamaresp = llamaresp + r
-                  line_count = line_count + len([ x for x in r if x in ['\n','\r']])
+                  self.line_count = self.line_count + len([ x for x in r if x in ['\n','\r']])
+
+                  linebuff = linebuff + r
+                  linebuff = self.parse_each_line(linebuff, md=md)
+
 
                   print(r, end="")
                   sys.stdout.flush()
 
                except Exception as e:
                   print(e)
+                  raise e
+
+      linebuff += "\n"
+      linebuff = self.parse_each_line(linebuff, md=md)
 
       if md:
-         self.rewrite_to_md(llamaresp, line_count)
+         self.rewrite_to_md(llamaresp, self.line_count)
 
       self.ctx.append(llamaresp)
       if not single:
@@ -213,7 +304,7 @@ def main():
 
    use_md = not args.plain
 
-   text = None
+   text = ""
 
    if args.text:
       text = " ".join(args.text)
